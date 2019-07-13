cbaacea5f8149795788789f1124c9cbe26ef3910
Naiyxx <52843714+Naiyxx@users.noreply.github.com>
2019-07-13 22:30:03 +0700

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,17 @@
+public class Quiz {
+    private String soal;
+    private String jawab;
+    public Quiz(String soal, String jawab) {
+        this.soal = soal;
+        this.jawab = jawab;
+    }
+    public boolean cocokanJawaban(String jawab) {
+        return this.jawab.equalsIgnoreCase(jawab);
+    }
+    public String getJawab() {
+        return jawab;
+    }
+    public String getSoal() {
+        return soal;
+    }
+}
